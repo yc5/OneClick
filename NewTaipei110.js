7637d67f3a75e8fac80c80c4785a1ecbd5470cdd
twstar <yc@xstar.me>
2017-04-22 10:33:08 +0800

    Add a new service
    New Taipei 110 form auto fill up.

---
+++ NewTaipei110.js
@@ -0,0 +1,13 @@
+javascript: var email = "<<Your Email>>",
+  id = "<<Your ID Number>>",
+  phone = "<<Yoru Ohine Number>>",
+  addr = "<<Your Address>>",
+  name = "<<Your Name>>";
+document.getElementById("event_name").value = name;
+document.getElementById("event_identity").value = id;
+document.getElementById("event_contact_address").value = addr;
+document.getElementById("event_phone").value = phone;
+document.getElementById("event_email").value = email;
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", "https://tvrs.ntpd.gov.tw/send_email.json?isCaptain=false&email=" + email, true);
+xhttp.send();
