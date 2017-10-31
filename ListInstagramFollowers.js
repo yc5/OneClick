f558473bc887980d154d583456c80b39d1985767
yc5 <yc@xstar.me>
2017-10-31 21:47:17 +0800

    Initialize

---
+++ ListInstagramFollowers.js
@@ -0,0 +1,7 @@
+for (
+  var i = 0;
+  i < document.querySelectorAll("._2g7d5.notranslate._o5iw8").length;
+  i++
+) {
+  console.log(document.querySelectorAll("._2g7d5.notranslate._o5iw8")[i].textContent);
+}
