108220c8883ff8f76133a1b815398ac94b629d1b
yc5 <yc@xstar.me>
2017-10-31 22:31:00 +0800

    With names.

--- ListInstagramFollowers.js
+++ ListInstagramFollowers.js
@@ -3,5 +3,9 @@ for (
   i < document.querySelectorAll("._2g7d5.notranslate._o5iw8").length;
   i++
 ) {
-  console.log(document.querySelectorAll("._2g7d5.notranslate._o5iw8")[i].textContent);
+  console.log(
+    document.querySelectorAll("._2g7d5.notranslate._o5iw8")[i].textContent +
+      " / " +
+      document.querySelectorAll("._9mmn5")[i].textContent
+  );
 }
