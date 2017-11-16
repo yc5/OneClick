dc069815022f2e0b3a20a5b46760188cf9377c63
yc5 <yc@xstar.me>
2017-11-16 09:14:37 +0800

    Redesign for the brand new index web page
    Works on http://www.library.ntpc.gov.tw/

--- NewTaipeiLibrary.js
+++ NewTaipeiLibrary.js
@@ -1,4 +1,13 @@
 javascript:
-document.getElementById("user").value = "<<Your ID>>";
-document.getElementById("password").value = "<<Your Password>>";
-document.getElementById("TextField").focus();
+if(window.location.href.includes("loginControl")){
+	try{
+		$("input[name=username]").val("<<Your ID>>");
+		$("input[name=password]").val("<<Your Password>>");
+		$("input[name=codenumber]").val($("input#codeVal").val());
+		$("input[type=submit]").click();
+	}catch(err){
+		alert(err.message);
+	}
+}else{
+	window.location.href = "http://www.library.ntpc.gov.tw/loginControl/";
+}
