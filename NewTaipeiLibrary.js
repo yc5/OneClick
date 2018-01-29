e78ed2e0f153ad4ea904f7f9efc0b0a808256524
yc5 <yc@xstar.me>
2018-01-29 20:54:41 +0800

    Brand new solution

--- NewTaipeiLibrary.js
+++ NewTaipeiLibrary.js
@@ -1,17 +1,25 @@
 javascript:
-if(window.location.href.includes("loginControl")){
-	try{
-		$("input[name=username]").val("<<Your ID>>");
-		$("input[name=password]").val("<<Your Password>>");
-		$("input[name=codenumber]").val($("input#codeVal").val());
-		$("input[type=submit]").click();
-	}catch(err){
-		alert(err.message);
+if(window.location.href.includes("Login.page")){
+	var PH = [
+		{"o": "user", "n": "userName", "v": "<<Your ID>>"},
+		{"o": "password", "n": "password", "v": "<<Your Password>>"},
+		{"o": "Hidden", "n": "redirectPage", "v": "opac/Search"},
+		{"o": "Hidden_0", "n": "ssologin", "v": "true"},
+	];
+	for(var i in PH) {
+	    var d = document.querySelector("input[name=" + PH[i].o + "]");
+		d.value = PH[i].v;
+		d.name = PH[i].n;
 	}
+	document.querySelector("#form").submit();
 }else{
 	if(document.documentElement.innerText.indexOf("登出") != -1){
-		window.location.href = "http://webpac.tphcc.gov.tw/toread/opac/search";		
+		if(window.location.href.toLowerCase().indexOf("search") != -1){
+			document.querySelector("#q").focus();
+		}else{
+			window.location.href = "http://webpac.tphcc.gov.tw/toread/opac/search";	
+		}
 	}else{
-		window.location.href = "http://www.library.ntpc.gov.tw/loginControl/";		
+		window.location.href = "http://webpac.tphcc.gov.tw/toread/opac/Login.page";
 	}
 }
