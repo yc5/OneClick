fb98e033d5a2a261993bd5d278bcfdbe7276e9b5
yc5 <yc@xstar.me>
2018-05-03 03:03:06 +0800

    Update LoginYouBike_2018.js

--- LoginYouBike_2018.js
+++ LoginYouBike_2018.js
@@ -1,9 +1,17 @@
 javascript:
+var code = document.documentElement.outerHTML;
 if(window.location.href.includes("//wa.ntpc.youbike.com.tw/login")){
-	var f = document.forms[0];
-	f.phonenumber.value = "<<Your Phone Number>>";
-	f.password.value = "<<Your Password>>";
-	f.submit();
-}else{
-	window.location.href = "http://wa.ntpc.youbike.com.tw/login";
+  var f = document.forms[0];
+  f.phonenumber.value = "<<Your Phone Number>>";
+  f.password.value = "<<Your Password>>";
+  f.submit();
+}else if (code.indexOf("登出") > 0) {
+  if (window.location.href.includes("//wa.ntpc.youbike.com.tw/riding_records")) {
+    document.forms[0].cardnohash.options[1].selected = true;
+    document.forms[0].submit();
+  } else {
+    window.location.href = "http://wa.ntpc.youbike.com.tw/riding_records";
+  }
+} else {
+  window.location.href = "http://wa.ntpc.youbike.com.tw/login";
 }
