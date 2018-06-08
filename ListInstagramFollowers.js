33c8540e4cf61943aefe84dedfed874618d50945
yc5 <yc@xstar.me>
2018-06-08 08:10:21 +0800

    Update for the new Instagram changes

--- ListInstagramFollowers.js
+++ ListInstagramFollowers.js
@@ -1,11 +1,11 @@
-for (
-  var i = 0;
-  i < document.querySelectorAll("._2g7d5.notranslate._o5iw8").length;
-  i++
-) {
-  console.log(
-    document.querySelectorAll("._2g7d5.notranslate._o5iw8")[i].textContent +
-      " / " +
-      document.querySelectorAll("._9mmn5")[i].textContent
-  );
+var item = document.querySelectorAll(".NroHT");
+var list = "";
+for (var i = 0; i < item.length; i++) {
+  list +=
+    item[i].querySelectorAll("a")[1].textContent +
+    " / " +
+    item[i].querySelectorAll("div")[4].textContent +
+    "<br>";
 }
+var myWindow = window.open("", "Fans List", "width=500,height=800");
+myWindow.document.write(list);
