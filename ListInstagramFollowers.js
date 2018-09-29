6976846c162a81da0b42e072a95cbb79907d67c4
yc5 <yc@xstar.me>
2018-09-29 16:07:20 +0800

    Update ListInstagramFollowers.js

--- ListInstagramFollowers.js
+++ ListInstagramFollowers.js
@@ -1,10 +1,10 @@
-var item = document.querySelectorAll("[role=dialog]>div:nth-child(3)>ul>div>li");
+var item = document.querySelectorAll("[role=dialog]>div:nth-child(2)>ul>div>li");
 var list = "";
 for (var i = 0; i < item.length; i++) {
   list +=
-    item[i].querySelectorAll("a")[1].textContent +
+    item[i].querySelector("div>div>div:nth-child(2)>div>a").textContent +
     " / " +
-    item[i].querySelectorAll("div")[4].textContent +
+    item[i].querySelector("div>div>div:nth-child(2)>div:nth-child(2)").textContent +
     "<br>";
 }
 var myWindow = window.open("", "Fans List", "width=500,height=800");
