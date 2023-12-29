c87bbc2dbd6564bb5e340f4baf9ab2a5e4d108d9
yc5 <yc@xstar.me>
2023-12-29 12:32:02 +0800

    Create TicketPlus-Captcha.js

---
+++ TicketPlus-Captcha.js
@@ -0,0 +1,32 @@
+javascript:
+var svgElement = document.querySelector(".captcha-img svg");
+var img = new Image();
+img.src = "data:image/svg+xml;utf8," + encodeURIComponent(svgElement.outerHTML);
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+
+img.onload = function () {
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, 0, 0);
+
+  var pngDataURL = canvas.toDataURL("image/png");
+  console.log(pngDataURL);
+
+  fetch("http://127.0.0.1:9898/ocr/b64/text", {
+    method: "POST",
+    body: pngDataURL.split("base64,")[1],
+  })
+    .then((res) => res.text())
+    .then((json) => {
+      console.log(json);
+      document.querySelector(".v-text-field__slot input").value = json;
+      document
+        .querySelector(".v-text-field__slot input")
+        .dispatchEvent(new Event("input"));
+      document.querySelector(".nextBtn").click();
+    });
+};
