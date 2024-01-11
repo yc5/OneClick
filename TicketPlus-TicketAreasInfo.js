9eddb354f4db7c4304d1054023a83eab489ab7cb
yc5 <yc@xstar.me>
2024-01-11 14:33:06 +0800

    fix: wrap await with async

--- TicketPlus-TicketAreasInfo.js
+++ TicketPlus-TicketAreasInfo.js
@@ -1,30 +1,47 @@
+javascript:
 var s3Url = "https://apis.ticketplus.com.tw/config/api/v1/getS3";
 var apiUrl = "https://apis.ticketplus.com.tw/config/api/v1";
-var eventId = window.location.pathname.match(/[^\/]+/g)[1];
-var ticketAreas, ticketAreaId, products, productId, params = {};
-var res = await axios.get(`${s3Url}?path=event/${eventId}/ticketAreas.json`);
-ticketAreas = res.data.ticketAreas;
-ticketAreaId = ticketAreas.map(x => x.ticketAreaId);
-params.ticketAreaId = ticketAreaId.join(",");
 
-res = await axios.get(`${s3Url}?path=event/${eventId}/products.json`);
-products = res.data.products;
-productId = products.map(x => x.productId);
-params.productId = productId.join(",");
+async function getavailableProducts() {
+    let currentUrlMatch = window.location.pathname.match(/[^\/]+/g);
+    let eventId = currentUrlMatch[1];
+    let sessionId = currentUrlMatch[2];
+    let ticketAreas, ticketAreaId, products, productId, params = {};
+    let res = await axios.get(`${s3Url}?path=event/${eventId}/ticketAreas.json`);
+    ticketAreas = res.data.ticketAreas.filter(x => x.sessionId === sessionId);
+    ticketAreaId = ticketAreas.map(x => x.ticketAreaId);
+    params.ticketAreaId = ticketAreaId.join(",");
 
-params._ = Date.now();
+    res = await axios.get(`${s3Url}?path=event/${eventId}/products.json`);
+    products = res.data.products.filter(x => x.sessionId === sessionId);;
+    productId = products.map(x => x.productId);
+    params.productId = productId.join(",");
 
-res = await axios.get(`${apiUrl}/get?${new URLSearchParams(params).toString()}`);
-productsStatus = res.data.result.ticketArea;
-availableProducts = productsStatus.filter(x => x.count > 0);
-availableProducts = availableProducts.map(x => {
-    let obj = {};
-    obj.ticketAreaName = x.ticketAreaName;
-    obj.price = x.price;
-    obj.count = x.count;
-    return obj
-});
-console.table(availableProducts);
+    res = await axios.get(`${apiUrl}/get?${new URLSearchParams(params).toString()}`);
+    ticketAreas = res.data.result.ticketArea;
+    if (ticketAreas.every(x => x.status === "pending")) {
+        let d = new Date();
+        console.warn(`Tickets Pending ${d.toLocaleTimeString()}`);
+        window.location.reload();
+    } else if (ticketAreas.every(x => x.status === "soldout")) {
+        let d = new Date();
+        console.warn(`Tickets Soldout ${d.toLocaleTimeString()}`);
+        window.location.reload();
+
+    } else {
+        ticketAreas = ticketAreas.filter(x => x.count > 0);
+        ticketAreas = ticketAreas.map(x => {
+            let obj = {};
+            obj.ticketAreaName = x.ticketAreaName;
+            obj.price = x.price;
+            obj.count = x.count;
+            return obj
+        });
+        console.table(ticketAreas);
+
+    }
+}
+getavailableProducts();
 
 
 var payload = {
